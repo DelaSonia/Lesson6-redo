77309f1ae17a62d42dc2843a335f60f8ad6890d4
DelaSonia <dela.sonia58@gmail.com>
2024-06-24 15:15:36 +0000

    Javascript

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@
 // alert("Hi "  + name1 +  " is my name");
 
 alert('Hi, whats your title');
-let title1 = prompt ('What is your title');
+let title1 = prompt ('I prefer to be addressed as');
 alert(title1);
 alert('What your fullname');
-let name1 = prompt('what is your name');
+let name1 = prompt('My full name is');
 alert(name1)
 alert('What is your favorite product');
-let product = prompt('What is your favorite product');
+let product = prompt('My favorite product is');
 alert(product)  
 alert(`Hi my name is ${title1} ${name1} and my favourite product is ${product}`);
