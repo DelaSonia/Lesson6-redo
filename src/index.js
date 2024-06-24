e3644a6830d47602dd767c3d2503f893c6180ecc
DelaSonia <dela.sonia58@gmail.com>
2024-06-24 13:07:18 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// alert('Hi, my name is Dela.');
+// let name1 = prompt('How are you doing');
+// alert(name1);
+// alert("Hi "  + name1 +  " is my name");
+
+alert('Hi, whats your title');
+let title1 = prompt ('What is your title');
+alert(title1);
+alert('What your fullname');
+let name1 = prompt('what is your name');
+alert(name1)
+alert('What is your favorite product');
+let product = prompt('What is your favorite product');
+alert(product)  
+alert(`Hi my name is ${title1} ${name1} and my favourite product is ${product}`);
